4cbd07108e625660a6740a7c5eacc4875663db39
edithngundi <edithngundi@gmail.com>
2023-12-03 14:30:52 +0800

    FEA-23-CoinMagnetPowerUps
    Introduced a magnet range which restricts the distance of the power up's function. Only coins within this range move towards the player

--- Assets/Scripts/MagnetPowerup.cs
+++ Assets/Scripts/MagnetPowerup.cs
@@ -8,19 +8,6 @@ public class MagnetPowerup : MonoBehaviour
     public AudioClip magnetSound;
     private float volume = 1.0f;
     public float rotationSpeed = 2.0f;
-    // Player's position
-    public Transform player;
-    public bool isActive = false;
-
-    void MagnetIsOn()
-    {
-        isActive = true;
-    }
-
-    void MagnetIsOff()
-    {
-        isActive = false;
-    }
 
     void Update()
     {
@@ -32,29 +19,26 @@ public class MagnetPowerup : MonoBehaviour
     {
         // Play the coin sound
         AudioSource.PlayClipAtPoint(magnetSound, transform.position, volume);
+
+        GameObject magnetPowerup = GameObject.FindWithTag("MagnetPowerup");
+        Destroy(magnetPowerup);
         // If the collider is the magnet powerup
         if(otherObject.tag == "Player")
         {
-            isActive = true;
-            
-            Vector3 playerPosition = new Vector3(PlayerController.instance.playerPosX, PlayerController.instance.playerPosY, PlayerController.instance.playerPosZ);
-            GameObject magneticCoin = GameObject.FindWithTag("MagneticCoin");
-            if (isActive == true)
+            GameObject[] magneticCoins = GameObject.FindGameObjectsWithTag("MagneticCoin");
+
+            foreach (GameObject magneticCoin in magneticCoins)
             {
-                StartCoroutine(MoveCoinToPlayer(magneticCoin, playerPosition));
-            } 
-        } 
-    }
-    IEnumerator MoveCoinToPlayer(GameObject magneticCoin, Vector3 playerPosition)
-    {
-        float speed = 3f;
-        while (Vector3.Distance(magneticCoin.transform.position, playerPosition) > 0.1f)
-        {
-            magneticCoin.transform.position = Vector3.Lerp(magneticCoin.transform.position, playerPosition, speed*Time.deltaTime);
-            yield return null;
+                
+                MagneticCoinController magneticCoinController = magneticCoin.GetComponent<MagneticCoinController>();
+                if (magneticCoinController != null)
+                {
+                    // Set the coin to active
+                    magneticCoinController.isActive = true;
+                    // Set the coin's speed
+                    magneticCoinController.SetSpeed(PlayerController.instance.racingSpeed);             
+                }
+            }
         }
-        GameObject magnetPowerup = GameObject.FindWithTag("MagnetPowerup");
-        // Destroy the powerup
-        Destroy(magnetPowerup);
-    }   
+    }
 }
--- Assets/Scripts/MagneticCoinController.cs
+++ Assets/Scripts/MagneticCoinController.cs
@@ -7,14 +7,48 @@ public class MagneticCoinController : MonoBehaviour
     // Add a variable to store the coin sound
     public AudioClip coinSound;
     private float volume = 1.0f;
-    // Update is called once per frame
-    // Player's position
-    public Transform player;
+
+    public bool isActive = false;
+
+    private float racingSpeed;
+
+    private GameObject player;
+
+    private float speed = 10.0f;
+
+    private int magnetRange = 30;
+
+    public void SetSpeed(float newSpeed)
+    {
+        racingSpeed = newSpeed;
+    }
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
 
     void Update()
     {
         // Rotate the coin along the x-axis
         transform.Rotate(100 * Time.deltaTime, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        if (isActive)
+        {
+            Vector3 playerPosition = new Vector3(PlayerController.instance.playerPosX, PlayerController.instance.playerPosY, PlayerController.instance.playerPosZ);
+            float distance = Vector3.Distance(transform.position, playerPosition);
+            if (distance <= magnetRange)
+            {
+                transform.position = Vector3.Lerp(transform.position, playerPosition, speed * Time.deltaTime);
+            }
+            else
+            {
+                isActive = false;
+            }          
+        }
     }
 
     private void OnTriggerEnter(Collider otherCollider)
@@ -24,13 +58,15 @@ public class MagneticCoinController : MonoBehaviour
         // If the collider is the player
         if(otherCollider.tag == "Player")
         {
+            // Disable the coin's collider
+            GetComponent<Collider>().enabled = false;
+
             // Increase the number of coins collected
             PlayerManager.coinsCollected += 1;
 
             // Destroy the coin
-            //Destroy(gameObject);
-            Debug.Log("Coin Destroyed");
-            
+            Destroy(gameObject);
+            Debug.Log("Coin Destroyed");           
         }
     }
 }
