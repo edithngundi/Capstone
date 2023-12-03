d31f6d72bfa163588bb4bc286ff7e0f2e8b98963
edithngundi <edithngundi@gmail.com>
2023-12-03 15:54:08 +0800

    FEA-21-SlideMovement
    Changed the slide movement implementation to crouching

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -47,6 +47,12 @@ public class PlayerController : MonoBehaviour
     public float playerPosY;
     public float playerPosZ;
 
+    // Crouch variables
+    public float crouchSpeed = 0.1f;
+    private float crouchYScale = 0.5f;
+    private float startYScale;
+    private bool isCrouching = false;
+
     public static PlayerController instance;
 
     void Awake()
@@ -64,6 +70,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        // Save the normal y-scale of the player
+        startYScale = transform.localScale.y;
     }
 
     /// <summary>
@@ -92,22 +100,28 @@ public class PlayerController : MonoBehaviour
             // No gravity when grounded
             movementDirection.y = -1;
             // Movement up
-            if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
             {
                 // Play the jump sound
                 AudioSource.PlayClipAtPoint(jumpSound, transform.position, volume);
                 CharacterJump();
             }
-            // Movement down
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            // Crouch movement
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             {
-                StartCoroutine(Roll());
+                isCrouching = true;
+                StartCoroutine(Crouch());
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+            {
+                isCrouching = false;
+                StartCoroutine(StandUp());
             }
         }
         else
         {
             // Movement down
-            if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
             {
                 // Play the land sound
                 AudioSource.PlayClipAtPoint(landSound, transform.position, volume);
@@ -122,7 +136,7 @@ public class PlayerController : MonoBehaviour
         }
 
         // Movement to the right
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             // Play the swerve sound
             AudioSource.PlayClipAtPoint(swerveSound, transform.position, volume);
@@ -131,7 +145,7 @@ public class PlayerController : MonoBehaviour
                 positionOnTrack = 2;
         }
         // Movement to the left
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             // Play the swerve sound
             AudioSource.PlayClipAtPoint(swerveSound, transform.position, volume);
@@ -165,42 +179,6 @@ public class PlayerController : MonoBehaviour
             characterController.Move(difference);
     }
 
-    IEnumerator Roll()
-    {
-        Quaternion originalRotation = transform.rotation;
-        Quaternion targetRotation = originalRotation * Quaternion.Euler(0, 0, -90);
-
-        Debug.Log(positionOnTrack);
-
-
-        transform.rotation = targetRotation;
-        if (transform.rotation == targetRotation)
-        {
-            // Turn off Y-axis capsule collider
-            GetComponent<CapsuleCollider>().direction = 1;
-            GetComponent<CapsuleCollider>().enabled = false;
-
-            // Turn on X-axis capsule collider
-            GetComponent<CapsuleCollider>().direction = 0;
-            GetComponent<CapsuleCollider>().enabled = true;
-        }
-
-        yield return new WaitForSeconds(0.5f);
-
-        transform.rotation = originalRotation;
-        if (transform.rotation == originalRotation)
-        {
-            // Turn off X-axis capsule collider
-            GetComponent<CapsuleCollider>().direction = 0;
-            GetComponent<CapsuleCollider>().enabled = false;
-
-            // Turn on Y-axis capsule collider
-            GetComponent<CapsuleCollider>().direction = 1;
-            GetComponent<CapsuleCollider>().enabled = true;
-        }
-    
-    }
-
     /// <summary>
     /// This method is responsible for moving the player. 
     /// Preferred to Update because it runs at a fixed rate/per delta time while Update runs per frame
@@ -215,13 +193,34 @@ public class PlayerController : MonoBehaviour
         characterController.Move(movementDirection * Time.fixedDeltaTime);
     }
 
+    IEnumerator Crouch()
+    {
+        while (transform.localScale.y > crouchYScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - crouchSpeed, transform.localScale.z);
+            yield return null;
+        }
+    }
+
+    IEnumerator StandUp()
+    {
+        while (transform.localScale.y < startYScale)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + crouchSpeed, transform.localScale.z);
+            yield return null;
+        }
+    }
+
     /// <summary>
     /// This method makes the player jump
     /// </summary>
     private void CharacterJump()
     {
-        movementDirection.y  = upwardJumpForce;
-        characterController.Move(movementDirection * Time.fixedDeltaTime);
+        if (!isCrouching)
+        {
+            movementDirection.y  = upwardJumpForce;
+            characterController.Move(movementDirection * Time.fixedDeltaTime);
+        }
     }
 
     private void CharacterFall()
